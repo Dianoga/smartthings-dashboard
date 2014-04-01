6700c2f3f7d9401d715ea0c97d9e5647a4a898eb
Brian Steere <brian-steere@uiowa.edu>
2014-04-01 10:12:36 -0500

    Things! Working towards an error message, but not there yet.

--- js/app.js
+++ js/app.js
@@ -8,8 +8,15 @@ angular
 
 		$scope.devices.$on('ready', function() {
 			angular.forEach($scope.devices, function(value, key) {
+				if (value == undefined) {
+					return;
+				}
+
 				if (value.temperature != undefined) {
 					setColorTemps(key, value.temperature);
+				}
+				if (value.battery != undefined) {
+					setBatteryLevel(key, value.battery);
 				}
 			});
 		});
@@ -17,11 +24,19 @@ angular
 		$scope.devices.$on('set', {
 			bubble: true
 		}, function(value, context) {
+			var key = context.key.split('/')[2];
 			if (context.key.indexOf('temperature') != -1) {
-				var key = context.key.split('/')[2];
 				setColorTemps(key, value);
+			} else if (context.key.indexOf('battery') != -1) {
+				setBatteryLevel(key, value);
 			}
 		});
+
+		$scope.devices.$on('error', {
+			bubble: true
+		}, function(err) {
+			console.log(err);
+		})
 
 		var setColorTemps = function(key, temp) {
 			var scale = chroma.scale(['#08519c', '#c6dbef', '#ffffb2', '#bd0026']).mode('hsv').domain([0, 100]).out('hex');
@@ -30,4 +45,16 @@ angular
 			$scope.devices[key].temperatureTextColor = textColor;
 			$scope.devices[key].temperatureBgColor = bgColor;
 		};
+
+		var setBatteryLevel = function(key, level) {
+			var icon = 'icon-battery-full';
+			if (level <= 25) {
+				icon = 'icon-battery-25';
+			} else if (level <= 50) {
+				icon = 'icon-battery-50';
+			} else if (level <= 75) {
+				icon = 'icon-battery-75';
+			}
+			$scope.devices[key].batteryIcon = icon;
+		}
 	});
